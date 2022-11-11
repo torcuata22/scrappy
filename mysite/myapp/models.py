600996f1e9d33d4b984f175685198d5ee0180323
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-11 15:33:30 -0600

    model to db

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+    address = models.CharField(max_length=2000, null=True) #because certain links may not have a working address
+    name = models.CharField(max_length=2000, null=True)
+
+    def __str__(self) -> str:
+        return self.name
