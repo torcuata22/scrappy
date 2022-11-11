2ba1588f4b5a690c8be10a57d3e69941ea174e7e
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-11 16:18:14 -0600

    delete and input

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,20 +2,32 @@ from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
 from .models import Link
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def scrape(request):
-    page = requests.get('http://www.google.com')
-    soup = BeautifulSoup(page.text, 'html.parser')
+    if request.method =="POST":
+        site = request.POST.get('site', '')
+
+    #Pass site to page:
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
     
 
-    for link in soup.find_all('a'):
-        link_address = (link.get('href'))
-        link_text = link.string
-        #create object based on Model, passing variables as parameters:
-        Link.objects.create(address = link_address, name = link_text)
-    #extract data from database:
-    data = Link.objects.all()
+        for link in soup.find_all('a'):
+            link_address = (link.get('href'))
+            link_text = link.string
+            #create object based on Model, passing variables as parameters:
+            Link.objects.create(address = link_address, name = link_text)
+        return HttpResponseRedirect('/')
+
+    else:
+        #extract data from database:
+        data = Link.objects.all()
 
     return render(request, 'myapp/result.html', {"data":data})
+
+def clear(request):
+    Link.objects.all().delete() #delete alll objects
+    return render(request, 'myapp/result.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,4 +20,6 @@ from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.scrape, name='scrape'),
+    path('delete/',views.clear, name='clear'),
+
 ]
